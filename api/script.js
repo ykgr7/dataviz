a3c8f2b5767909084fa3aa02259fdf87e0486dea
Benkein <benkeingaming@gmail.com>
2022-10-12 15:28:46 +0200

    on avance comme on peut hein

--- api/script.js
+++ api/script.js
@@ -6,7 +6,19 @@ d3.json("/data.json")
     .data(datajson)
     .enter()
     .append("div")
-    .text(d => `numéro ${d.rank}: ${d.personName} à ${d.city}`);
-
-    
+    .text(d => `numéro ${d.rank}: ${d.personName} à ${d.city}`)
+    .attr("class","rich");
 });
+
+const scrollerOutput = document.querySelector(".scrolloutput");
+const pageheight = document.body.clientHeight;
+const screenheight = window.innerHeight;
+
+document.addEventListener("scroll", (event) => {
+    let scrollpercentage = Math.round((scrollY*100)/screenheight)
+    let scrollCase = 0
+    scrollerOutput.textContent = `scroll: ${scrollpercentage}% ${scrollCase}`;
+  });
+
+
+//   let scrollpercentage = Math.round((scrollY*100)/(pageheight-screenheight))
