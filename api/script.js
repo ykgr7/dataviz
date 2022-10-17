8dd1ad2e4df1a769c4085283514cc6fad273cd15
Benkein <benkeingaming@gmail.com>
2022-10-17 16:03:07 +0200

    Slg (salut les geeks hahahahahaha🤣🤣🤣😂😂😃🤣🤣😂)

--- api/script.js
+++ api/script.js
@@ -1,14 +1,44 @@
+
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
+
+
+
+
+
 //recup de données JSON et mise en forme.
 d3.json("/data.json")
 .then(datajson => {
 
-    d3.select(".lesbourges")
-    .selectAll("div")
-    .data(datajson)
-    .enter()
-    .append("div")
-    .text(d => `numéro ${d.rank}: ${d.personName} à ${d.city} avec ${Math.round(d.finalWorth*10)/10} Billion dollars`)
-    .attr("class","rich");
+    // d3.select(".lesbourges")
+    // .selectAll("div")
+    // .data(datajson)
+    // .enter()
+    // .append("div")
+    // .text(d => `numéro ${d.rank}: ${d.personName} à ${d.city} avec ${Math.round(d.finalWorth*10)/10} Billion dollars`)
+    // .attr("class","rich");
 
 
       
